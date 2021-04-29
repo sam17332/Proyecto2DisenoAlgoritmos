99848563aa3df55ff19e6a00f1ccdca18c9b7da9
toti <sam17332@uvg.edu.gt>
2021-04-29 15:23:53 -0600

    Cambios en funciones para lograr el funcionamiento de la simulacion + se borra codigo innecesario

--- scanner.py
+++ scanner.py
@@ -1,13 +1,14 @@
+
 import pickle5 as pickle
-from pprint import pprint as pp
+import pprint
 
 class Scanner:
     def __init__(self, documentoALeer):
-        self.diccioSiguientePos = {}
         self.diccioAceptacion = {}
         self.pilaFinal = []
         self.cadenaALeer = ""
         self.nombreDocALeer = documentoALeer
+        self.print = pprint.PrettyPrinter()
 
     def main(self):
         self.openFiles()
@@ -23,40 +24,15 @@ class Scanner:
         self.pilaFinal = pickle.load(file)
         file.close()
 
-        file = open("diccioSiguientePos", "rb")
-        self.diccioSiguientePos = pickle.load(file)
-        file.close()
-
         file = open("diccioAceptacion", "rb")
         self.diccioAceptacion = pickle.load(file)
         file.close()
-
-    def getFinalNumber(self):
-        arrayLocal = []
-        for id, value in self.diccioSiguientePos.items():
-            if len(value) == 0 and id not in arrayLocal:
-                arrayLocal.append(id)
-
-        return arrayLocal
-
-    def getEstadosFinales(self):
-        finales = []
-        numeroEstadoFinal = self.getFinalNumber()
-        for estado in self.pilaFinal:
-            for estadoFinal in numeroEstadoFinal:
-                if(str(estadoFinal) in estado[1] and estado[1] not in finales):
-                    finales.append(estado[0])
-
-        return finales
 
     def getStateNumber(self, array):
         for valor in self.pilaFinal:
             if(valor[1] == array):
                 return valor[0]
 
-    """
-    Funcion para encontrar el siguiente estado
-    """
     def mover(self, estados, caracter):
         array = []
         for estado in estados:
@@ -75,52 +51,78 @@ class Scanner:
 
     def getToken(self, estados):
         token = ""
-        print(self.diccioAceptacion)
         for transicion in self.pilaFinal:
             for estado in estados:
-                print("estado")
-                print(estado)
                 if(estado == transicion[0]):
                     for estadoInd in transicion[1]:
-                        print("estadoInd")
-                        print(estadoInd)
-                        if(estadoInd in self.diccioAceptacion):
-                            token = self.diccioAceptacion[estado]
+                        for key, valor in self.diccioAceptacion.items():
+                            if(int(estadoInd) == int(key)):
+                                token = valor
+
+                                return token
 
         return token
 
     def simular(self):
         cadena = []
         s = [0]
+        s2 = [0]
         cont = 0
+        tokenDef = ""
         for i in self.cadenaALeer:
             cadena.append(i)
         cadena.append(" ")
 
         while len(cadena) > 0:
-            char = self.cadenaALeer[cont]
-            print(char)
-            sBefore = s
-            s = self.mover(s, char)
-            print(s)
-
-            if(len(s) == 0):
-                token = self.getToken(sBefore)
+            # Si no se esta en el ultimo caracter de la cadena
+            if(cont == len(self.cadenaALeer)-1):
+                char = self.cadenaALeer[cont]
+                tokenDef += char
+                s = self.mover(s, char)
+                token = self.getToken(s)
                 if(len(token) == 0):
-                    print("Token invalido")
+                    self.print.pprint("La cadena " + str(tokenDef) + " es un token invalido!")
+                    break
+                self.print.pprint("La cadena " + str(tokenDef) + " es el token " + str(token))
+                break
+
+            char1 = self.cadenaALeer[cont]
+            char2 = self.cadenaALeer[cont+1]
+            tokenDef += char1
+            s = self.mover(s, char1)
+            s2 = self.mover(s, char2)
+
+            # Si el caracter siguiente no tiene transicion,
+            # pero el caracter  actual si
+            if(len(s2) == 0 and len(s) > 0):
+                token = self.getToken(s)
+                # Si se encontro un token
+                if(len(token) == 0):
+                    self.print.pprint("La cadena " + tokenDef + " es un token invalido!")
                     s = [0]
+                    s2 = [0]
+                    tokenDef = ""
                     cont -= 1
                 else:
-                    print("El token encontrado es" + str(token))
+                    self.print.pprint("La cadena " + tokenDef + " es el token " + token)
+                    s = [0]
+                    s2 = [0]
+                    tokenDef = ""
+            # Si no hay transicion
+            elif(len(s) == 0):
+                self.print.pprint("La cadena " + tokenDef + " es un token invalido!")
+                s = [0]
+                s2 = [0]
+                tokenDef = ""
+
             cont += 1
             cadena.pop()
 
-
 def menu():
-    # nombre = str(input("Ingrese el nombre del archivo que desea leer"))
-    nombre = "lectura.txt"
+    nombre = str(input("Ingrese el nombre del archivo que desea leer: "))
 
     main = Scanner(nombre)
     main.main()
 
 menu()
+            
